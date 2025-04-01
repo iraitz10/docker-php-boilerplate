6578b891c9384d2083c82175a80c1ba14651f850
Iraitz Aiesa Rekarte <149527391+iraitz10@users.noreply.github.com>
2025-04-01 12:53:27 +0200

    Test unitario de añadir producto a la lista

--- tests/ShoppingListTest.php
+++ tests/ShoppingListTest.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace Deg540\DockerPHPBoilerplate\Test;
+
+use PHPUnit\Framework\TestCase;
+use Deg540\DockerPHPBoilerplate\ShoppingList;
+
+class ShoppingListTest extends TestCase
+{
+    private ShoppingList $list;
+
+    protected function setUp(): void {
+        $this->list = new ShoppingList();
+    }
+
+    /**
+     * @test
+     */
+    public function givenItemAddToList(): void {
+        $this->assertEquals("pan x1", $this->list->process("añadir pan"));
+        $this->assertEquals("leche x2, pan x1", $this->list->process("añadir leche 2"));
+        $this->assertEquals("leche x2, pan x3", $this->list->process("añadir Pan 2"));
+    }
+}
